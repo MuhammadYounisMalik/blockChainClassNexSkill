47fbd5b0113f4b9e7a14dbc270c25244a13e3f01
MuhammadYounisMalik <114486916+MuhammadYounisMalik@users.noreply.github.com>
2022-11-01 21:10:32 +0500

    Initial commit

---
+++ malik.mjs
@@ -0,0 +1,4 @@
+console.log("Imran Khan")
+alert('imran Khan')
+let MyName="Muhammad Younis Malik";
+document.write(MyName);
---
+++ template/blockchainsecond.mjs
@@ -0,0 +1,12 @@
+/*let malik=56;
+var raza = "Muhammad Younis Malik"
+const pi =3.1415;
+pi=1.9;
+
+console.log(pi);*/
+
+
+let nro1=12;
+var nro2=8;
+const PI =3.1415;
+document.write(nro1 + "<br>" + "<br>" nro2 + PI );
